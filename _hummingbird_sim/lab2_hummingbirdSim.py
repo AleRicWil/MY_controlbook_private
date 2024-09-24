f8ea866d6011be1562025698e20ae4f3080769ca
AleRicWil <alericwil@gmail.com>
2024-09-24 16:36:18 -0600

    Working Animation

--- _hummingbird_sim/lab2_hummingbirdSim.py
+++ _hummingbird_sim/lab2_hummingbirdSim.py
@@ -6,9 +6,9 @@ from hummingbirdAnimation import HummingbirdAnimation
 from dataPlotter import DataPlotter
 
 # instantiate reference input classes
-phi_ref = SignalGenerator(amplitude=1.5, frequency=0.05)
-theta_ref = SignalGenerator(amplitude=0.5, frequency=0.05)
-psi_ref = SignalGenerator(amplitude=0.5, frequency=.05)
+phi_ref = SignalGenerator(amplitude=1.5, frequency=0.2)
+theta_ref = SignalGenerator(amplitude=0.5, frequency=0.2)
+psi_ref = SignalGenerator(amplitude=0.5, frequency=0.2)
 
 # instantiate the simulation plots and animation
 dataPlot = DataPlotter()
@@ -18,13 +18,13 @@ t = P.t_start  # time starts at t_start
 while t < P.t_end:  # main simulation loop
     # set variables
     phi = phi_ref.sin(t)
-    theta = 0#theta_ref.sin(t)
-    psi = 0#psi_ref.sin(t)
+    theta = 0#theta_ref.sin(t*5)
+    psi = 0#psi_ref.sin(t*5)
     # update animation
     state = np.array([[phi], [theta], [psi], [0.0], [0.0], [0.0]])
     ref = np.array([[0], [0], [0]])
-    force = 0
-    torque = 0
+    force = 10
+    torque = 10
     pwms = np.array([[force], [torque]])
     animation.update(t, state)
     dataPlot.update(t, state, pwms, ref)
