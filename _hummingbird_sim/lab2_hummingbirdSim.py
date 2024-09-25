e20b961a108508de98b66380ff179d701c459d79
AleRicWil <alericwil@gmail.com>
2024-09-24 18:36:24 -0600

    noen

--- _hummingbird_sim/lab2_hummingbirdSim.py
+++ _hummingbird_sim/lab2_hummingbirdSim.py
@@ -6,7 +6,7 @@ from hummingbirdAnimation import HummingbirdAnimation
 from dataPlotter import DataPlotter
 
 # instantiate reference input classes
-phi_ref = SignalGenerator(amplitude=1.5, frequency=0.2)
+phi_ref = SignalGenerator(amplitude=1.0, frequency=0.2)
 theta_ref = SignalGenerator(amplitude=0.5, frequency=0.2)
 psi_ref = SignalGenerator(amplitude=0.5, frequency=0.2)
 
@@ -18,8 +18,8 @@ t = P.t_start  # time starts at t_start
 while t < P.t_end:  # main simulation loop
     # set variables
     phi = phi_ref.sin(t)
-    theta = 0#theta_ref.sin(t*5)
-    psi = 0#psi_ref.sin(t*5)
+    theta = theta_ref.sin(t)
+    psi = psi_ref.sin(t)
     # update animation
     state = np.array([[phi], [theta], [psi], [0.0], [0.0], [0.0]])
     ref = np.array([[0], [0], [0]])
